10b95efe060b46bc9072e5f472cabe5e910369df
IamMonkie <43730745+IamMonkie@users.noreply.github.com>
2018-11-11 21:49:50 -0500

    Revision A
    Remaining Problems:
    1) for loop not determining win or loss state
    2) id=playerScore ignoring css

---
+++ assets/javascript/game.js
@@ -0,0 +1,139 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------
+//runs at startup
+--------------------------------------------------------------------------------------------------------------------------------------------*/
+$(document).ready(function () {
+
+//reset on page load
+window.onload = function () {
+    reset();
+}
+
+/*------------------------------------------------------------------------------------------------------------------------------------------
+//Variables
+--------------------------------------------------------------------------------------------------------------------------------------------*/
+
+let gemValue1 = 0;
+let gemValue2 = 0;
+let gemValue3 = 0;
+let gemValue4 = 0;
+
+let targetValue = 0;
+
+let playerArray = [];
+let playerValue = 0;
+
+let wins = 0;
+let losses = 0;
+
+/*------------------------------------------------------------------------------------------------------------------------------------------
+//scripts
+--------------------------------------------------------------------------------------------------------------------------------------------*/
+//Gem Buttons
+$('#gemBox1').on("click", function () {
+   playerArray.push(gemValue1);
+   getSum();
+   console.log("Gem Value 1: " + gemValue1 + '\n' +
+   "target Value: " + targetValue + '\n' +
+   "player Value: " + playerValue);
+   
+
+});
+
+$('#gemBox2').on("click", function () {
+    playerArray.push(gemValue2);
+    getSum();
+    console.log("Gem Value 2: " + gemValue2 + '\n' +
+   "target Value: " + targetValue + '\n' +
+   "player Value: " + playerValue);
+    
+});
+
+$('#gemBox3').on("click", function () {
+    playerArray.push(gemValue3);
+    getSum();
+    console.log("Gem Value 3: " + gemValue3 + '\n' +
+   "target Value: " + targetValue + '\n' +
+   "player Value: " + playerValue);
+    
+});
+
+$('#gemBox4').on("click", function () {
+    playerArray.push(gemValue4);
+    getSum();
+    console.log("Gem Value 4: " + gemValue4 + '\n' +
+    "target Value: " + targetValue + '\n' +
+    "player Value: " + playerValue); 
+    
+});
+
+//wins/ losses
+
+
+    if(playerValue === targetValue) {
+        wins++;
+        document.getElementById("winsText").textContent = "WINS: " + wins;
+        swal("THAT'S IT MORTY, WE DID IT. ", "PRESS RESET TO PLAY AGAIN", "success");
+        reset();
+        
+    }
+    
+    else if(playerValue > targetValue) {
+        losses++;
+        document.getElementById("lossesText").textContent = "LOSSES: " + losses;
+        swal("AAW CRAP MORTY...YOU...YOU'VE REALLY GONE AND DONE IT THIS TIME. GUESS WERE STUCK HERE", "PRESS RESET TO PLAY AGAIN", "error");
+        reset();
+    }    
+
+
+//Reset Button
+$("#resetBtn").on("click", function () { 
+    reset();
+})
+
+/*------------------------------------------------------------------------------------------------------------------------------------------
+//Functions
+--------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//player score
+function getSum() {
+    playerValue = playerArray.reduce(function(a,b){
+        let sum = a + b;
+        return sum;
+    })
+updatePlayer();
+}
+    
+//play again       
+function reset() {
+        
+    //assign Random Values to buttons
+    gemValue1 = Math.floor(Math.random() * 12) + 1;
+    gemValue2 = Math.floor(Math.random() * 12) + 1;
+    gemValue3 = Math.floor(Math.random() * 12) + 1;
+    gemValue4 = Math.floor(Math.random() * 12) + 1;
+
+    //reset energy
+    playerValue = 0;
+    playerArray = [];
+
+    //assign target value
+    targetValue = Math.floor(Math.random() * 120) + 19;
+    updateTarget();
+
+    //reset energy gathered
+    updatePlayer();
+            
+};
+
+//update target
+function updateTarget() {
+    document.getElementById("targetScore").textContent = "Energy Required: " + targetValue;
+}
+//update player
+function updatePlayer() { 
+    document.getElementById("playerScore").textContent = "Energy Gathered: " + playerValue;
+}    
+
+}); // ready close
+
+
